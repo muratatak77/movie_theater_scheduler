c1e5ab57caf6bdd3a8ee9ad6a7caec103bf64200
Murat Atak <muratatak77@gmail.com>
2021-01-25 10:54:39 -0800

    update operatin tab size

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -5,16 +5,16 @@ require 'enums'
 
 class Operation
 
-	attr_accessor :weekday_show_times, :weekend_show_times, :movie_duration
+  attr_accessor :weekday_show_times, :weekend_show_times, :movie_duration
 
-	def initialize(weekday_show_times: [], weekend_show_times: [], movie_duration: 0)
-		@movie_duration = movie_duration
-		@weekday_show_times = create_weekday_list
-		@weekend_show_times = create_weekend_list
-	end
+  def initialize(weekday_show_times: [], weekend_show_times: [], movie_duration: 0)
+    @movie_duration = movie_duration
+    @weekday_show_times = create_weekday_list
+    @weekend_show_times = create_weekend_list
+  end
 
-	def create_weekend_list
-  	opening_hour = get_weekend_hours[0]
+  def create_weekend_list
+    opening_hour = get_weekend_hours[0]
     closing_hour = get_weekend_hours[1]
     get_hours_list(opening_hour,closing_hour)
   end
@@ -22,24 +22,24 @@ class Operation
   def create_weekday_list
     opening_hour = get_weekday_hours[0]
     closing_hour = get_weekday_hours[1]
-		get_hours_list(opening_hour,closing_hour)
+    get_hours_list(opening_hour,closing_hour)
   end
 
   private 
 
   def get_hours_list(opening_hour, closing_hour)
-  	default_movie_operation_time = Enums::PREPATE_TIME + Enums::PREVIEW_TIME
+    default_movie_operation_time = Enums::PREPATE_TIME + Enums::PREVIEW_TIME
     total_working_minute = DateUtils.second_to_min(closing_hour - opening_hour)
     max_video_count_per_day = get_max_video_count_per_day(total_working_minute, default_movie_operation_time, @movie_duration)
     hours_result = []
     previous_movie_time = 0
     for i in (max_video_count_per_day).downto(1)
       if i == max_video_count_per_day
-      	#first video we can movie duration minute from extract closing hour 
+        #first video we can movie duration minute from extract closing hour 
         start_movie_time = closing_hour - @movie_duration.minutes
       else
-      	#first another videos, we can add default movie operation time + movie duration
-      	#and we can extract previous movie time  to find next start movie time
+        #first another videos, we can add default movie operation time + movie duration
+        #and we can extract previous movie time  to find next start movie time
         start_movie_time_diff = default_movie_operation_time + @movie_duration
         start_movie_time = previous_movie_time - start_movie_time_diff.minutes
       end
